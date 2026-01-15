1690a388e669b37276ad8dd6b807246fd1f42f6c
adorobantu92 <dorobantu29@gmail.com>
2026-01-15 02:33:34 +0200

    Wire cart provider globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { CartProvider } from "@/lib/cart-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +39,7 @@ export default function RootLayout({
             </Link>
           </div>
         </header>
-        {children}
+        <CartProvider>{children}</CartProvider>
       </body>
     </html>
   );
